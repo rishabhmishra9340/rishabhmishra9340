f17ae8fd7973c6b715421adc6f80b25538555d6e
rishabhmishra9340 <104819756+rishabhmishra9340@users.noreply.github.com>
2022-05-15 22:29:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import Counter from'./Component/Counter'
-import Dataset from './Component/Dataset';
-import Header from './Component/Header';
-import Input from './Component/Input';
-import Output from './Component/Output';
+
 import React from "react";
 import MenuCard from './Component/MenuCard';
 import Resturant from './Component/Resturant';
 import './Component/Style.css'
 import Menu from './Component/MenuApi';
 import Navbar from './Component/NavBar';
-import './Component/practice';
-import Sansi from './Sansi'
+
 function App() {
   return (
     <div className="App">
